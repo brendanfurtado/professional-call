ed1508ce09cd24a9aa6197a10f073640d117bb08
brendanfurtado <brenfurtado@gmail.com>
2023-08-29 00:47:10 -0400

    Added Prisma and created foundation for profiles and db

---
+++ lib/initial-profile.ts
@@ -0,0 +1,35 @@
+import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+
+export const initialProfile = async () => {
+  const user = await currentUser();
+
+  // If the user is not signed in, redirect to sign in
+  if (!user) {
+    return redirectToSignIn();
+  }
+
+  // If the user is already signed in, check if they have a profile
+  const profile = await db.profile.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (profile) {
+    return profile;
+  }
+
+  // If the user is signed in and doesn't have a profile, create one
+  const newProfile = await db.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
+
+  return newProfile;
+};
