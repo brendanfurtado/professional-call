95861105dc7e29c246ec8d4c8d1cb9b250efe143
brendanfurtado <brenfurtado@gmail.com>
2023-09-03 16:04:47 -0400

    added invite users functionality

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,63 @@
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+
+interface InviteCodePageProps {
+  params: {
+    inviteCode: string;
+  };
+}
+//Add user to server then redirect them to the server page
+const InviteCodePage = async ({ params }: InviteCodePageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  if (!params.inviteCode) {
+    return redirect("/");
+  }
+
+  //If user exists in server, redirect them to the server page
+  const existingServer = await db.server.findFirst({
+    where: {
+      inviteCode: params.inviteCode,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  //Redirect here
+  if (existingServer) {
+    return redirect(`/servers/${existingServer.id}`);
+  }
+
+  //Otherwise, add user to server
+  const server = await db.server.update({
+    where: {
+      inviteCode: params.inviteCode,
+    },
+    data: {
+      members: {
+        create: [
+          {
+            profileId: profile.id,
+          },
+        ],
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return null;
+};
+
+export default InviteCodePage;
